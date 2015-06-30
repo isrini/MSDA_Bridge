07193c1b2f827c552584159c9928e82ed956cfe7
isrini <ipsr.contact@gmail.com>
2015-06-30 11:37:40 -0400

    MSDA Bridge Program
    Week 1 for R

---
+++ Week1.R
@@ -0,0 +1,31 @@
+# 1. Write a loop that calculates 12-factorial.
+fact <- function(x){
+  x = 12
+  y <- 1
+  for(i in 1:x){
+    y <-y*((1:x)[i])
+    print(y)
+  }
+  
+}
+
+fact(12)
+
+# 2. Show how to create a numeric vector that contains the sequence from 20 to 50 by 5
+numVec <- c(20:50)
+numVec[seq(1,length(numVec),5)]
+
+# 3. Show how to take a trio of input numbers a, b, and c and implement the quadratic equation.
+
+a <- 2
+b <- 7
+c <- 5
+d <- sqrt((b^2 - 4*a*c))
+
+x <- (-b + d)/2*a
+# or
+x1 <- (-b - d)/2*a
+
+print(x)
+print(x1)
+
